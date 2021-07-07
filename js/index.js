7f970c7e92877be71757242d88d9834342478377
Marcela Duenas <analista.desarrollo@spia.com.co>
2021-07-07 13:14:04 -0500

    ejercicio js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+class animal {
+    constructor(especie, edad, color) {
+        this.especie = especie;
+        this.edad = edad;
+        this.color = color;
+    }
+}
+
+let perro = new animal("perro", 5, "rojo");
+console.log(perro)
+
+
+
 /**********************************************/
 /*  Set a Functions when window scrolls
 /**********************************************/
